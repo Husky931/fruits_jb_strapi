c20634ef2ec4c75c5db58ec71d0e5f4b0c8077ad
Xz33M Hask <pecevgligor@protonmail.com>
2023-11-22 16:01:28 +0800

    test ci

--- config/server.js
+++ config/server.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
   //   url: '/admin',
   //   serveAdminPanel: true,
   // },
-  // hi
+  // cron
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
